0096c85ee9d7685f5c87a6656848003731eb19c4
DESKTOP-B2SQKVQ\User <saily.d8@gmail.com>
2020-12-29 21:09:37 -0500

    updated properties

---
+++ src/main/java/com/reminder/app/configuration/DatabaseConfig.java
@@ -0,0 +1,15 @@
+package com.reminder.app.configuration;
+
+//@Configuration
+public class DatabaseConfig {
+
+//  @Value("${spring.datasource.url}")
+//  private String dbUrl;
+//
+//  @Bean
+//  public DataSource dataSource() {
+//      HikariConfig config = new HikariConfig();
+//      config.setJdbcUrl(dbUrl);
+//      return new HikariDataSource(config);
+//  }
+}
